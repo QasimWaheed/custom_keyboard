650e9e58d9268adfde733e91f014264d50e2d23e
Qasim Waheed <qasimwaheed12344@gmail.com>
2022-11-06 10:35:12 +0500

    initial commit

---
+++ lib/custom_keyboard.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:keyboard_actions/keyboard_custom.dart';
+
+class TextKey extends StatelessWidget {
+  const TextKey({
+    Key? key,
+    required this.text,
+    this.onTextInput,
+    this.flex = 1,
+  }) : super(key: key);
+  final String text;
+  final Function(String)? onTextInput;
+  final int flex;
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: flex,
+      child: Padding(
+        padding: const EdgeInsets.all(1.0),
+        child: Material(
+          // color: Colors.blue.shade300,
+          child: InkWell(
+            onTap: onTextInput != null ? () {
+              onTextInput!(text);
+            } : null,
+            child: Center(child: Text(text, style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w500),)),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomKeyboard extends StatelessWidget with KeyboardCustomPanelMixin<String> implements PreferredSizeWidget {
+  const CustomKeyboard({
+    Key? key,
+    required this.notifier,
+  }) : super(key: key);
+
+  final ValueNotifier<String> notifier;
+
+  void _textInputHandler(text) {
+    updateValue("${notifier.value}$text");
+  }
+
+  void _backspace() {
+    if (notifier.value.isEmpty) return;
+    var length = notifier.value.length-1;
+    if (length == 0) {
+      updateValue("");
+      return;
+    }
+    var value = notifier.value.substring(0, length);
+    updateValue(value);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: preferredSize.height,
+      color: Colors.grey[200],
+      child: Column(        // <-- Column
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                TextKey(
+                  text: '1',
+                  onTextInput: _textInputHandler,
+                ),
+                TextKey(
+                  text: '2',
+                  onTextInput: _textInputHandler,
+                ),
+                TextKey(
+                  text: '3',
+                  onTextInput: _textInputHandler,
+                ),
+              ],
+            ),
+          ),    // <-- Row
+          Expanded(
+            child: Row(
+              children: [
+                TextKey(
+                  text: '4',
+                  onTextInput: _textInputHandler,
+                ),
+                TextKey(
+                  text: '5',
+                  onTextInput: _textInputHandler,
+                ),
+                TextKey(
+                  text: '6',
+                  onTextInput: _textInputHandler,
+                ),
+
+              ],
+            ),
+          ),    // <-- Row
+          Expanded(
+            child: Row(
+              children: [
+                TextKey(
+                  text: '7',
+                  onTextInput: _textInputHandler,
+                ),
+                TextKey(
+                  text: '8',
+                  onTextInput: _textInputHandler,
+                ),
+                TextKey(
+                  text: '9',
+                  onTextInput: _textInputHandler,
+                ),
+              ],
+            ),
+          ),  // <-- Row
+          Expanded(
+            child: Row(
+              children: [
+                TextKey(
+                  text: '.',
+                  onTextInput: _textInputHandler,
+                ),
+                TextKey(
+                  text: '0',
+                  onTextInput: _textInputHandler,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(1.0),
+                    child: Material(
+                      // color: Colors.blue.shade300,
+                      child: InkWell(
+                        onTap: _backspace,
+                        child: const Center(
+                          child: Icon(Icons.backspace, size: 30,),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),  // <-- Row
+
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(200);
+}
---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:keyboard_actions/keyboard_actions.dart';
+import 'custom_keyboard.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final custom1Notifier = ValueNotifier<String>("");
+  final FocusNode _nodeText1 = FocusNode();
+
+  KeyboardActionsConfig _buildConfig(BuildContext context) {
+    return KeyboardActionsConfig(
+      keyboardActionsPlatform: KeyboardActionsPlatform.ALL,
+      keyboardBarColor: Colors.grey[200],
+      nextFocus: false,
+      actions: [
+        KeyboardActionsItem(
+          focusNode: _nodeText1,
+          footerBuilder: (_) => CustomKeyboard(
+            notifier: custom1Notifier,
+          ),
+        ),
+      ],
+    );
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: KeyboardActions(
+        config: _buildConfig(context),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Get.bottomSheet(Material(
+            child: KeyboardCustomInput<String>(
+              focusNode: _nodeText1,
+              height: 65,
+              notifier: custom1Notifier,
+              builder: (context, val, hasFocus) {
+                return Container(
+                  alignment: Alignment.centerLeft,
+                  color: hasFocus! ? Colors.grey[300] : Colors.white,
+                  child: Text(
+                    val,
+                    // style:
+                    // TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                );
+              },
+            ),
+          ));
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
